d0a605ab3fe123d998cbd09b0f668803bbf5bb2e
Michael Bucher <michael@axlabs.com>
2023-10-12 14:07:32 +0200

    (#3) Add todo for overwrite functionality of validators and relayer

--- contracts/BridgeContract.sol
+++ contracts/BridgeContract.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.9;
 contract Bridge {
     
     // To set when compiling contract for loading its bytes into genesis script.
+    // Todo: Discuss using values hardcoded here as default and add an overwrite functionality.
     address public constant relayer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    address[7] public validators = [
+    address[] public validators = [
         0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
         0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC,
         0x90F79bf6EB2c4f870365E785982E1f101E93b906,
