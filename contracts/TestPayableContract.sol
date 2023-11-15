6c42f60cab134e831eaf787e3efc3cd5cfceb902
cloud8little <413074538@qq.com>
2023-11-15 11:10:57 +0800

    update

--- contracts/TestPayableContract.sol
+++ contracts/TestPayableContract.sol
@@ -4,5 +4,4 @@ pragma solidity ^0.8.9;
 // This Contract is only used to test HashTreeBridgeContract
 contract TestPayableContract {
     receive() external payable {}
-    fallback() external payable {}
 }
