b3b8d2b1ea2340ef74943ad87ce96be2fc8de211
Michael Bucher <michael@axlabs.com>
2024-03-27 23:31:49 +0100

    (#38) Add hardhat-upgrades dependency to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 import '@typechain/hardhat'
+import '@openzeppelin/hardhat-upgrades'
 import '@nomicfoundation/hardhat-chai-matchers'
 import 'hardhat-storage-layout'
 
