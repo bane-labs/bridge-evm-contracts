6d1262aef358569eb1e4ceab7ed9435cf2b69673
moonpyt <moon1179797619@gmail.com>
2024-05-27 15:10:18 +0800

    add unit test

---
+++ contracts/tests/TestBridge.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../bridge/BridgeImpl.sol";
+
+contract TestBridge is BridgeImpl {
+    constructor(address m) BridgeImpl(m) {
+    }
+
+
+    function getTokenConfig (
+        address neoXToken
+    ) public view returns (StorageTypes.TokenConfig memory config) {
+        config = _getTokenConfig(neoXToken);
+        return config;
+    }
+}
---
+++ test/Bridge.t.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "../lib/forge-std/src/Test.sol";
+import {TestBridge} from "../contracts/tests/TestBridge.sol";
+import "../contracts/bridge/BridgeStorage.sol";
+import "../contracts/management/BridgeManagementImpl.sol";
+import "../contracts/tests/SigUtils.sol";
+
+
+contract BridgeImplTest is Test,SigUtils {
+    // constructor(address _management) BridgeImpl(_management) {}
+    TestBridge bridgeImpl;
+
+    address neoXToken = address(0x6782);
+    address neoN3Token = address(0x7892);
+    StorageTypes.TokenConfig validConfig;
+
+    // set _management
+    BridgeManagementImpl bridgeManagementImpl;
+    SigUtils sigUtils;
+    address public owner = 0xBcd4042DE499D14e55001CcbB24a551F3b954096;
+    address public user = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public relayer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint8 public validatorThreshold = 5;
+    uint256[] public validatorsKeys;
+    address[] public validatorsAddresses;
+    address internal governor = 0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f;
+    address internal securityGuard = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720;
+
+
+
+    function setUp() public {
+        //set  _management
+        sigUtils = new SigUtils();
+        validatorsKeys.push(user0PrivateKey);
+        validatorsAddresses.push(vm.addr(user0PrivateKey));
+        validatorsKeys.push(user1PrivateKey);
+        validatorsAddresses.push(vm.addr(user1PrivateKey));
+        validatorsKeys.push(user2PrivateKey);
+        validatorsAddresses.push(vm.addr(user2PrivateKey));
+        validatorsKeys.push(user3PrivateKey);
+        validatorsAddresses.push(vm.addr(user3PrivateKey));
+        validatorsKeys.push(user4PrivateKey);
+        validatorsAddresses.push(vm.addr(user4PrivateKey));
+        validatorsKeys.push(user5PrivateKey);
+        validatorsAddresses.push(vm.addr(user5PrivateKey));
+        validatorsKeys.push(user6PrivateKey);
+        validatorsAddresses.push(vm.addr(user6PrivateKey));
+        bridgeManagementImpl = new BridgeManagementImpl(
+                owner,
+                relayer,
+                5,
+                validatorsAddresses,
+                governor,
+                securityGuard,
+                user
+            );
+        // vm.prank(owner);
+        // bridgeManagementImpl.setGovernor(governor);
+
+        // ready for register token dada
+
+        bridgeImpl = new TestBridge(address(bridgeManagementImpl));
+        validConfig = StorageTypes.TokenConfig({
+            neoN3Token: neoN3Token,
+            fee: 1,
+            minAmount: 100,
+            maxAmount: 1000,
+            maxDeposits: 10,
+            tokenType: StorageTypes.TokenType.NEO
+        });
+    }
+
+
+    function testRegisterToken() public {
+        vm.prank(governor);
+        // console.log("msg.sender: %s, token: %s",msg.sender,neoXToken);
+        bridgeImpl.registerToken(neoXToken, validConfig);
+        StorageTypes.TokenConfig memory config =  bridgeImpl.getTokenConfig(neoXToken);
+        assertEq(config.neoN3Token, neoN3Token);
+        assertEq(config.fee, 1);
+        assertEq(config.minAmount, 100);
+        assertEq(config.maxAmount, 1000);
+        assertEq(config.maxDeposits, 10);
+    }
+
+
+
+    // function testFailRegisterTokenWithZeroAddress() public {
+    //     vm.prank(governor);
+    //     bridgeImpl.registerToken(address(0), validConfig);
+    // }
+
+
+    // function testFailRegisterTokenWithInvalidAmount() public {
+    //     vm.prank(governor);
+    //     StorageTypes.TokenConfig memory invalidConfig = StorageTypes.TokenConfig({
+    //         neoN3Token: neoN3Token,
+    //         fee: 1,
+    //         minAmount: 1000,
+    //         maxAmount: 100,
+    //         maxDeposits: 10,
+    //         tokenType: StorageTypes.TokenType.NEO
+    //     });
+    //     bridgeImpl.registerToken(neoXToken, invalidConfig);
+    // }
+
+    // function testFailRegisterTokenWithInvalidNeoN3TokenAddress() public {
+    //     vm.prank(governor);
+    //     StorageTypes.TokenConfig memory invalidConfig = StorageTypes.TokenConfig({
+    //         neoN3Token: address(0),
+    //         fee: 1,
+    //         minAmount: 100,
+    //         maxAmount: 1000,
+    //         maxDeposits: 10,
+    //         tokenType: StorageTypes.TokenType.NEO
+    //     });
+    //     bridgeImpl.registerToken(neoXToken, invalidConfig);
+    // }
+
+    // function testFailRegisterTokenAlreadyRegistered() public {
+    //     vm.prank(governor);
+    //     bridgeImpl.registerToken(neoXToken, validConfig);
+    //     bridgeImpl.registerToken(neoXToken, validConfig); // Should fail
+    // }
+}
