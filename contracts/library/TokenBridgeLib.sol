49e621e0ccf3365a06705dc0da40bf600c49f439
Alvan <1541115709@qq.com>
2024-05-30 12:16:42 +0800

    remove the code.length check

--- contracts/library/TokenBridgeLib.sol
+++ contracts/library/TokenBridgeLib.sol
@@ -19,7 +19,7 @@ library TokenBridgeLib {
     ) internal returns (bool) {
         bytes memory transferCall = abi.encodeCall(IERC20.transfer, (_to, _amount));
         (bool success, bytes memory returndata) = address(_neoXToken).call(transferCall);
-        return success && (returndata.length == 0 || abi.decode(returndata, (bool))) && address(_neoXToken).code.length > 0;
+        return success && (returndata.length == 0 || abi.decode(returndata, (bool)));
     }
 
     /**
