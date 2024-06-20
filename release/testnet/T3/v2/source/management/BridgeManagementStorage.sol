71dbaa4bba049114ffd17b6266ea979fed524902
Michael Bucher <michael@axlabs.com>
2024-06-20 14:48:29 +0200

    Remove genesis-only functions

--- release/testnet/T3/v2/source/management/BridgeManagementStorage.sol
+++ release/testnet/T3/v2/source/management/BridgeManagementStorage.sol
@@ -81,37 +81,4 @@ contract BridgeManagementStorage is UUPSUpgradeable {
     function _authorizeUpgrade(
         address newImplementation
     ) internal virtual override onlyAdmin {}
-
-    // UUPSUpgradeable-specific functions required for precompiled version.
-
-    /**
-     * @dev Reverts if the execution is not performed via delegatecall or the execution
-     * context is not of a proxy with an ERC-1967 compliant implementation pointing to self.
-     * See the modifier {onlyProxy} in UUPSUpgradeable.sol.
-     *
-     * Only for precompiled uups implementation in genesis file, need to be removed when upgrading the contract.
-     * This override is added because "immutable __self" in UUPSUpgradeable is not available in precompiled contract.
-     */
-    function _checkProxy() internal view virtual override {
-        if (
-            address(this) == SELF || // Must be called through delegatecall
-            ERC1967Utils.getImplementation() != SELF // Must be called through an active proxy
-        ) {
-            revert UUPSUnauthorizedCallContext();
-        }
-    }
-
-    /**
-     * @dev Reverts if the execution is performed via delegatecall.
-     * See the modifier {notDelegated} in UUPSUpgradeable.sol.
-     *
-     * Only for precompiled uups implementation in genesis file, need to be removed when upgrading the contract.
-     * This override is added because "immutable __self" in UUPSUpgradeable is not available in precompiled contract.
-     */
-    function _checkNotDelegated() internal view virtual override {
-        if (address(this) != SELF) {
-            // Must not be called through delegatecall
-            revert UUPSUnauthorizedCallContext();
-        }
-    }
 }
