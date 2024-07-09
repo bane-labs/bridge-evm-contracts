f64811ffaf0e4fce19b5dc9cf11ecb2823670206
Michael Bucher <michael@axlabs.com>
2024-07-09 12:21:53 +0200

    Add source and compile data for T4

---
+++ release/testnet/T4/source/interfaces/IBridge.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../library/BridgeLib.sol";
+
+interface IBridge {
+    // General bridge events
+
+    event BridgePause();
+    event BridgeUnpause();
+    event Fund(uint256 amount);
+
+    // General bridge functions
+
+    function pauseBridge() external;
+
+    function unpauseBridge() external;
+}
---
+++ release/testnet/T4/source/interfaces/IBridgeManagement.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../library/BridgeLib.sol";
+
+interface IBridgeManagement {
+    event OwnerChange(address owner);
+    event RelayerChange(address relayer);
+    event ValidatorsChange(address[] validators, uint256 threshold);
+    event GovernorChange(address governor);
+    event SecurityGuardChange(address securityGuard);
+    event FunderChange(address funder);
+
+    function setRelayer(address _relayer) external;
+
+    function getRelayer() external view returns (address);
+
+    function setValidators(
+        address[] calldata _validators,
+        uint256 threshold
+    ) external;
+
+    function getValidators() external view returns (address[] memory);
+
+    function getValidator(uint256 _index) external view returns (address);
+
+    function getValidatorThreshold() external view returns (uint256);
+
+    function verifyValidatorSignatures(
+        bytes32 _newDepositRoot,
+        BridgeLib.Signature[] calldata _signatures
+    ) external view returns (bool);
+
+    function setGovernor(address _governor) external;
+
+    function getGovernor() external view returns (address);
+
+    function setSecurityGuard(address _securityGuard) external;
+
+    function getSecurityGuard() external view returns (address);
+
+    function setFunder(address _funder) external;
+
+    function getFunder() external view returns (address);
+}
