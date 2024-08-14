ca1ce915c2dfbb84b0b1e0b3bf823f729f4991f7
Michael Bucher <michael@axlabs.com>
2024-08-14 17:18:09 +0200

    (#168) Update tests for integrating chainid into signature message

--- utils/signature-utils.ts
+++ utils/signature-utils.ts
@@ -1,16 +1,18 @@
 import { ethers } from "hardhat";
 import { Signer, Signature } from "ethers";
 
+const defaultChainId = 31337n;
+
 export async function getSignature(signer: Signer, msg: any): Promise<Signature> {
     return ethers.Signature.from(await signer.signMessage(msg));
 }
 
 /** 
- * @param msg the message to sign.
+ * @param root the root to use for creating the signature.
  * @param which the index of the validators to sign. Use 1-7 here. Index 0 represents the relayer.
  * @returns the promise of a signature array with the specified validator's signatures of the message.
  */
-export async function getValidatorSignatures(msg: any, which: number[] = [1, 2, 3, 4, 5]): Promise<Signature[]> {
+export async function getValidatorSignatures(root: any, which: number[] = [1, 2, 3, 4, 5]): Promise<Signature[]> {
     const [
         _,
         validator1,
@@ -21,6 +23,7 @@ export async function getValidatorSignatures(msg: any, which: number[] = [1, 2, 
         validator6,
         validator7
     ] = await ethers.getSigners();
+    let msg = ethers.getBytes(ethers.solidityPackedKeccak256(["uint256", "bytes32"], [defaultChainId, root]));
     let validators: Signer[] = [_, validator1, validator2, validator3, validator4, validator5, validator6, validator7];
     let sigs: Signature[] = [];
     for (let i of which) {
