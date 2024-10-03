e741cc521cf31dfeba94d461f20fbbe05434649a
Michael Bucher <michael@axlabs.com>
2024-10-03 14:53:40 +0200

    Add token registration scripts

---
+++ scripts/registerNeo.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import { N3_NEO_ADDRESS, getBridgeFromEnv, getNeoXTokenFromEnv } from "./utils/addresses";
+import { TokenExecutionType } from "./utils/constants";
+import { registerToken } from "./utils/registration";
+import { getDeployer, getOwner } from "./utils/wallet";
+import { fundIfLocalNetwork } from "./utils/network";
+
+async function main() {
+    const deployer = getDeployer(ethers.provider);
+    const governor = getOwner(ethers.provider);
+    fundIfLocalNetwork(deployer.address);
+    fundIfLocalNetwork(governor.address);
+    const bridge = await getBridgeFromEnv(ethers.provider);
+    const token = await getNeoXTokenFromEnv();
+    await registerToken(bridge, governor, token, TokenExecutionType.NEO, N3_NEO_ADDRESS);
+    await token.connect(deployer).transfer(await bridge.getAddress(), ethers.parseEther("10000"));
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
