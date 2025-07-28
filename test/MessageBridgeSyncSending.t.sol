83863c5784df3b0ff983d7106bbab70b734ec8e5
Michael Bucher <michael@axlabs.com>
2025-07-28 17:55:23 +0200

    chore: collect duplicate amb test code in helper

--- test/MessageBridgeSyncSending.t.sol
+++ test/MessageBridgeSyncSending.t.sol
@@ -11,7 +11,7 @@ import {IMessageBridge} from "../contracts/messageBridge/interfaces/IMessageBrid
 import {ReentrancyAttacker} from "../contracts/tests/ReentrancyAttacker.sol";
 import {SigUtils} from "../contracts/tests/SigUtils.sol";
 import {TestBridgeManagement} from "../contracts/tests/TestBridgeManagement.sol";
-import {TestMessageContract} from "../contracts/tests/TestMessageContract.sol";
+import {MessageBridgeTestHelper} from "./MessageBridgeTestHelper.sol";
 import {TestPayableContract} from "../contracts/tests/TestPayableContract.sol";
 import {CommonBase} from "../lib/forge-std/src/Base.sol";
 import {StdAssertions} from "../lib/forge-std/src/StdAssertions.sol";
@@ -22,86 +22,7 @@ import {Test} from "../lib/forge-std/src/Test.sol";
 import {Options} from "../lib/openzeppelin-foundry-upgrades/src/Options.sol";
 import {Upgrades} from "../lib/openzeppelin-foundry-upgrades/src/Upgrades.sol";
 
-contract MessageBridgeSyncSending is Test, SigUtils {
-    TestMessageContract messageBridgeProxy;
-    address messageBridgeProxyAddress;
-
-    // Management
-    TestBridgeManagement managementProxy;
-    address managementProxyAddress;
-    SigUtils sigUtils;
-    address public owner = 0xBcd4042DE499D14e55001CcbB24a551F3b954096;
-    address public funder = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    address public relayer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    uint8 public validatorThreshold = 7;
-    address[] public validatorsAddresses;
-    address internal governor = 0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f;
-    address internal securityGuard = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720;
-
-    ExecutionManager public executionManager;
-
-    // Message Bridge Config
-    uint256 messageFee = 0.01 ether;
-    uint256 maxMessageSize = 1024;
-    uint256 maxNrMessages = 10;
-    uint256 executionWindowSeconds = 60;
-
-    // Test message data
-    bytes testMessage1 =
-        abi.encode(AMBTypes.Call({allowFailure: false, target: address(0x1234), value: 0, callData: hex"abcd"}));
-    bytes testMessage2 =
-        abi.encode(AMBTypes.Call({allowFailure: true, target: address(0x5678), value: 0, callData: hex"ef01"}));
-
-    function setUp() public {
-        sigUtils = new SigUtils();
-        validatorsAddresses.push(vm.addr(user0PrivateKey));
-        validatorsAddresses.push(vm.addr(user1PrivateKey));
-        validatorsAddresses.push(vm.addr(user2PrivateKey));
-        validatorsAddresses.push(vm.addr(user3PrivateKey));
-        validatorsAddresses.push(vm.addr(user4PrivateKey));
-        validatorsAddresses.push(vm.addr(user5PrivateKey));
-        validatorsAddresses.push(vm.addr(user6PrivateKey));
-
-        // Allow constructor to bypass the safety check in deployUUPSProxy.
-        Options memory opts;
-        opts.unsafeAllow = "constructor";
-
-        // Deploy the bridge management implementation
-        managementProxyAddress = Upgrades.deployUUPSProxy(
-            "TestBridgeManagement.sol",
-            abi.encodeCall(
-                TestBridgeManagement.initialize,
-                (owner, relayer, validatorThreshold, validatorsAddresses, governor, securityGuard, funder)
-            ),
-            opts
-        );
-        managementProxy = TestBridgeManagement(payable(managementProxyAddress));
-        // Validate initialization to version 3
-        assertEq(managementProxy.getCurrentInitializedVersion(), 3);
-
-        // Deploy the MessageBridge implementation
-        messageBridgeProxyAddress = Upgrades.deployUUPSProxy(
-            "TestMessageContract.sol",
-            abi.encodeCall(
-                MessageBridge.initialize,
-                (managementProxyAddress, messageFee, maxMessageSize, maxNrMessages, executionWindowSeconds)
-            ),
-            opts
-        );
-        messageBridgeProxy = TestMessageContract(payable(messageBridgeProxyAddress));
-
-        // Deploy and set up the Message Executor
-        executionManager = new ExecutionManager(messageBridgeProxyAddress);
-
-        // Set the message executor in the bridge
-        vm.prank(governor);
-        messageBridgeProxy.setMessageExecutor(address(executionManager));
-
-        // Unpause the message bridge
-        vm.prank(governor);
-        messageBridgeProxy.unpauseMessageBridge();
-    }
-
+contract MessageBridgeSyncSending is MessageBridgeTestHelper {
     function test_hashMessageSendOp_executable() public view {
         uint256 nonce = 1;
         uint256 timestamp = 1753000000;
