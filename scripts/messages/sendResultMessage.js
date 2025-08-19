65e502697b1a5f6bd0951d6bff389d3c6b383bc2
Michael Bucher <michael@axlabs.com>
2025-08-19 10:15:22 +0200

    feat: add and update scripts for sending messages

--- scripts/messages/sendResultMessage.js
+++ scripts/messages/sendResultMessage.js
@@ -7,9 +7,9 @@ const { DEFAULT_TX_OVERRIDES } = require("../utils/constants");
 async function sendTestMessageResult(messageBridge) {
     const sender = getOwner(ethers.provider);
     await fundIfLocalNetwork([sender.address]);
-
     const nonce = 1;
-    await messageBridge.sendResultMessage(nonce, DEFAULT_TX_OVERRIDES);
+    const sendingFee = ethers.parseEther("0.1");
+    await messageBridge.sendResultMessage(nonce, {value: sendingFee}, DEFAULT_TX_OVERRIDES);
 }
 
 async function main() {
