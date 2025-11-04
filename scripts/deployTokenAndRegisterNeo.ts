ada9ea0169092040ee9be72f10a67b46e4b06e7a
Otniel Nicola <otniel@axlabs.com>
2025-11-04 09:50:00 +0200

    feat: Add scripts for setting bridges

--- scripts/deployTokenAndRegisterNeo.ts
+++ scripts/deployTokenAndRegisterNeo.ts
@@ -3,7 +3,7 @@ import { deployTokenContract } from "./deploy/token";
 import { getBridgeFromEnv, N3_NEO_ADDRESS } from "./utils/addresses";
 import { fundIfLocalNetwork } from "./utils/network";
 import { registerTokenWithScalingFactor } from "./utils/registration";
-import {getDeployer, getGovernor } from './utils/wallet';
+import { getDeployer, getGovernor } from "./utils/wallet";
 
 async function main() {
     const deployer = getDeployer(ethers.provider);
