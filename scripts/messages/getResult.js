56a10dd148980eb9b7d4fd5955cdf603ca172504
Otniel Nicola <otniel@axlabs.com>
2025-11-07 16:58:43 +0200

    feat: Add utility scripts for message manipulation

--- scripts/messages/getResult.js
+++ scripts/messages/getResult.js
@@ -1,19 +1,31 @@
-const { getMessageBridgeFromEnv } = require("../utils/addresses");
+const {getMessageBridgeFromEnv} = require('../utils/addresses');
 
 async function getExecutionResult(messageBridge, nonce) {
-    const { success, returnData } = await messageBridge.getEvmExecutionResult(nonce);
-    console.log("Execution result:");
+  try {
+    const {success, returnData} = await messageBridge.getEvmExecutionResult(nonce);
+    console.log('EVM Execution result:');
     console.log(success);
     console.log(returnData);
+  } catch (e) {
+    console.log('EVM Execution result not found for nonce', nonce);
+  }
+  const neoNonce = await messageBridge.getNeoExecutionResultNonce(nonce);
+  console.log('Corresponding NeoN3 execution result nonce:', neoNonce.toString());
+  const neoResult = await messageBridge.getNeoExecutionResult(neoNonce);
+  console.log('NeoN3 Execution result:');
+  console.log(neoResult);
 }
 
 async function main() {
-    const nonce = 1;
-    const messageBridge = await getMessageBridgeFromEnv();
-    await getExecutionResult(messageBridge, nonce);
+  const envNonce = process.env.NONCE || (() => {
+    throw new Error('Please set the NONCE environment variable');
+  })();
+  const nonce = parseInt(envNonce);
+  const messageBridge = await getMessageBridgeFromEnv();
+  await getExecutionResult(messageBridge, nonce);
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
